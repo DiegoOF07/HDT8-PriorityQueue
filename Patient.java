99ccad7914221858e233caf07e5280cebe5cf1a6
Diego Flores <floresdiego041@gmail.com>
2024-03-30 16:15:29 -0600

    Creating Patient class

---
+++ Patient.java
@@ -0,0 +1,43 @@
+public class Patient {
+    
+    private String name;
+    private String symptom;
+    private char emergencyCode;
+    
+    public Patient(String name, String symptom, char emergencyCode) {
+        this.name = name;
+        this.symptom = symptom;
+        this.emergencyCode = emergencyCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymptom() {
+        return symptom;
+    }
+
+    public void setSymptom(String symptom) {
+        this.symptom = symptom;
+    }
+
+    public char getEmergencyCode() {
+        return emergencyCode;
+    }
+
+    public void setEmergencyCode(char emergencyCode) {
+        this.emergencyCode = emergencyCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente: " + name + " | sintoma: " + symptom + " | codigo: " + emergencyCode;
+    }
+
+    
+}
